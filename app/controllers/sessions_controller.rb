e98b0f493ffb770bb4d1de0a86325d1cf5763af8
wiliajc87 <jordythepoet@gmail.com>
2015-02-01 21:14:32 -0600

    adding base resources

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202020707) do
+ActiveRecord::Schema.define(version: 20150202030043) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
