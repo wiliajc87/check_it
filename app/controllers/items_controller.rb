f5820ef6bed819c73b7f5ac66c59ece60a8ba9ba
wiliajc87 <jordythepoet@gmail.com>
2015-03-22 18:26:02 -0500

    added a destroy method for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,32 @@
 class ItemsController < ApplicationController
+  def index
+    @user = User.find(session[:user_id])
+    @items = @user.items
+    @movies = @items.where(:media_type => "movie")
+    @books = @items.where(:media_type => "book")
+    @music = @items.where(:media_type => "music")
+  end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(user_id: session[:user_id], name: params[:item][:name], media_type: params[:item][:media_type])
     @item.find_link
-    @item.save
-    redirect_to items_path
+    if @item.save
+      redirect_to items_path
+    else
+    end
   end
 
   def show
     @item = params[:item_id]
   end
 
-  def index
-    @items = Item.all
+  def destroy
+    @item = Item.find(params[:id])
+    @item.delete
+    redirect_to items_path
   end
 end
